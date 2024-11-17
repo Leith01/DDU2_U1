566ee2c65fe0b965cbc0d1487584e363b1c87cc1
Leith01 <leith.216@gmail.com>
2024-11-17 18:43:28 +0100

    Påbörjade u1 med att koppla filerna inom html  samt skappade variabler utifrån befintliga html-element i js

--- website/index.js
+++ website/index.js
@@ -1,6 +1,14 @@
 // Recommended: All functions declared here
+function createAllCityBox() {
+    
+}
 
 // Recommended: constants with references to existing HTML-elements
+const cities = document.querySelector("#cities");
+const target = document.querySelector(".target");
+const closest = document.querySelector(".closest");
+const furthest = document.querySelector(".furthest");
+const table = document.querySelector("#table");
 
 // Recommended: Ask for the city name and then the rest of the code
 
