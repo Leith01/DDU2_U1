c90c860af9f868332519263ac68ae3c0c7ae233d
Leith01 <leith.216@gmail.com>
2024-11-18 12:49:14 +0100

    Fixa till en sök funktion för alla städernas namn när man promptar en stad, gjorde även att namnen visas eller inte visas på hemsidan!

--- website/index.js
+++ website/index.js
@@ -1,7 +1,24 @@
 // Recommended: All functions declared here
-function createAllCityBox() {
-    
+
+//En funktion för att initiera sidan
+function init() {
+    createAllCityBox();
+    createDistanceTable();
 }
+
+function searchCity(name) {
+    for (let i = 0; i < cities.length; i++) {
+        if (cities[i].name === name) {
+            return cities[i];
+        }
+    }
+}
+
+function createAllCityBox() {
+    //.....
+}
+
+
 
 // Recommended: constants with references to existing HTML-elements
 const cities = document.querySelector("#cities");
@@ -11,4 +28,22 @@ const furthest = document.querySelector(".furthest");
 const table = document.querySelector("#table");
 
 // Recommended: Ask for the city name and then the rest of the code
+
+const targetCityName = prompt("Vilken stad?")
+
+const h2Element = document.querySelector("h2");
+
+if (!targetCityName) {
+    alert("Du angav ingen stad. Try again!")
+    h2Element.textContent = "Ingen stad angavs";
+    return;
+}
+
+const targetCity = searchCity(targetCityName);
+if (targetCity) {
+    h2Element.textContent = '${targetCity.name} (${targetCity.country})';
+}
+else {
+    h2Element.textContent = "Staden hittades inte!";
+}
 
