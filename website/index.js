2984610d202af030584b2c02d5868a6822fa69ae
Leith Ahmad <leith.216@gmail.com>
2024-11-20 23:45:50 +0100

    Implementera funtkion för att beräkna distanser av städerna samt påbörja med att markera stad boxarna!
    La till funktion "findDistanceToCity" som beräknar den närmaste och längst bort staden från en angiven stad (targetCity).
    Funktionen går igenom alla avstån mellan städer (lagrade i "distances" innuti databasen) och jämför avstånden för att hitta det närmaste och längst bort avstånd för den valda staden.
    Startvärden för closeDistance och farDistance är satt till 0 för att tillåta exakta avstånd (inklusive 0km om det är samma stad) men främst för att jämföra städerna exakta distanser.
    För varje avstånd jämförs om det är mindre än det nuvarande kortaste eller största avståndet, och den närmaste och längst bort staden uppdateras därefter.
    Efter beräkningen ska det uppdatera användargränssnittet för att markera den valda staden, samt den närmaste och längst bort staden, i HTML elementet h3.

--- website/index.js
+++ website/index.js
@@ -8,20 +8,20 @@ function init() {
 }
 
 function addCity() {
-    const targetCityName = prompt("Vilken stad?")
-
-    const h2Element = document.querySelector("h2");
-
+    
     if (!targetCityName) {
         h2Element.textContent = "Ingen stad angavs";
+        title.textContent = "Not found"
         return;
     }
 
     const targetCity = searchCity(targetCityName);
     if (targetCity) {
         h2Element.textContent = `${targetCity.name} (${targetCity.country})`;
+        title.textContent = `${targetCity.name}`;
     } else {
         h2Element.textContent = "Staden hittades inte!"; 
+        title.textContent = "Not found";
     }
 }
 
@@ -31,6 +31,7 @@ function searchCity(name) {
             return cities[i];
         }
     }
+    return null;
 }
 
 function createAllCityBox() {
@@ -46,13 +47,75 @@ function createAllCityBox() {
     addCity();
 }
 
+function findDistanceToCity(targetCity) {
+    let closestCity = null; //null eftersom att vi inte har något objekt att jämföra med
+    let furthestCity = null;  //null eftersom att vi inte har något objekt att jämföra med
+    let closeDistance = 0;
+    let farDistance = 0;
+
+    for (let i = 0; i < distances.length; i++) {
+        const distance = distances[i].distance;
+        const city1Id = distances[i].city1;
+        const city2Id = distances[i].city2;
+
+        if (city1Id === targetCity.id) {
+            if (distance < closeDistance || closeDistance === 0) {
+                closeDistance = distance;
+                closestCity = cities[city2Id]
+            }
+            if (distance > farDistance || farDistance === 0) {
+                farDistance = distance;
+                furthestCity = cities[city2Id];
+            }
+        }
+
+        else if (city2Id === targetCity.id) {
+            if (distance < closeDistance || closeDistance === 0) {
+                closeDistance = distance;
+                closestCity = cities[city1Id];
+            }
+            if (distance > farDistance || farDistance === 0) {
+                farDistance = distance;
+                furthestCity = cities[city1Id]
+            }
+        }
+    }
+    return {closestCity, furthestCity, closeDistance, farDistance};
+}
+
+function markTargetCity(targetCity) {
+    const cityBoxes = document.getElementsByClassName("cityBox");
+    const targetCity = searchCity(targetCityName)
+
+    for (let i = 0; i < cityBoxes.length; i++) {
+        const cityBox = cityBoxes[i];
+        const city = cities[i];
+
+        if (city.id === targetCity.id) {
+            cityBox[i].classList.add("target");
+            cityBox.textContent = `${city.name}`;
+        }
+
+        else if (city.id == closestCity.id) {
+           // cityBox[i].textContent = 
+        }
+    }
+}
+
+
+
 // Recommended: constants with references to existing HTML-elements
 const cities_id = document.querySelector("#cities");
 const target = document.querySelector(".target");
 const closest = document.querySelector(".closest");
 const furthest = document.querySelector(".furthest");
 const table = document.querySelector("#table");
-
+const title = document.querySelector("title");
+const h2Element = document.querySelector("h2");
 // Recommended: Ask for the city name and then the rest of the code
+
+const targetCityName = prompt("Vilken stad?")
+
+
 
 init();
